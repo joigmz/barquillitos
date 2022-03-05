f846abe61460821bc302abb731d48ab42a20c4e9
joigmz <jose.izam99@gmail.com>
2022-03-05 18:36:43 -0300

    navbar with automatic active class

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 window.onscroll = function () {
     myFunction();
+    myFunction2();
 };
 
 function myFunction() {
@@ -8,8 +9,32 @@ function myFunction() {
     if (window.pageYOffset <= img_pos_botton) {
         document.getElementById("options").classList.remove(
           "fixed");
+
     } else {
         document.getElementById("options").classList.add(
           "fixed");
     }
+
+}
+
+function myFunction2() {
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll(".category");
+
+    let current = "";
+    sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+    }
+
+    navLi.forEach((a) => {
+        a.classList.remove("active");
+        if (a.classList.contains(current)) {
+            a.classList.add("active");
+        }
+    });
+    });
+
 }
