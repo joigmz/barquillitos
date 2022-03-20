f785c40af1faf80ae7782ca3c8899f1537d4e669
joigmz <jose.izam99@gmail.com>
2022-03-20 18:20:52 -0300

    cart option

--- static/js/main.js
+++ static/js/main.js
@@ -56,3 +56,20 @@ function lockScroll() {
     root.classList.toggle('lock-scroll');
 
 }
+
+function increaseCount(e, el) {
+    var input = el.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+  }
+function decreaseCount(e, el) {
+    var input = el.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
+    }
+}
